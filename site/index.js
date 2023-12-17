3bf965ffb147cd4fee427a84b5321955ad788c6c
Davide Poletti <dav.poletti@gmail.com>
2023-12-18 00:35:13 +0100

    versione 1

--- site/index.js
+++ site/index.js
@@ -1,3 +1,12 @@
 import("./node_modules/wasm-game-of-life/wasm_game_of_life.js").then((js) => {
-    js.greet("WebAssembly with npm");
+    const pre = document.getElementById("game-of-life-canvas");
+    const universe = js.Universe.new();
+    const renderLoop = () => {
+      pre.textContent = universe.render();
+      universe.tick();
+    
+      requestAnimationFrame(renderLoop);
+    };
+    requestAnimationFrame(renderLoop);
+
   });
